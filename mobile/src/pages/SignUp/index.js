77ef7e908eb1709f6d7c02f9a58a9ef241958a97
renatopcinfo <renatopcinfo@gmail.com>
2019-11-25 00:00:45 -0300

    first commit

---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,67 @@
+import React, {useRef} from 'react';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({navigation}) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnkeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-adrress"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnkeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnkeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho conta</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
