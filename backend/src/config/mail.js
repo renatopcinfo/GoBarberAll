f72b45c9e633c8e7bf54c2807143ed0a1fee6182
renatopcinfo <renatopcinfo@gmail.com>
2019-12-13 00:32:47 -0300

    email config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2951d03d7db57a',
+    pass: 'b5ee5a2fbd0bc7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
