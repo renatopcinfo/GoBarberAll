51a0ef4740223c7e11854f4c5548001aa61ef35d
renatopcinfo <renatopcinfo@gmail.com>
2019-12-22 00:26:11 -0300

    env

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '2951d03d7db57a',
-    pass: 'b5ee5a2fbd0bc7',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
--- backend/src/config/sentry.js
+++ backend/src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://9628006d42fd49b8b8d9d96434e0c657@sentry.io/1864141',
+  dsn: process.env.SENTRY_DSN,
 };
+// 'https://9628006d42fd49b8b8d9d96434e0c657@sentry.io/1864141'
