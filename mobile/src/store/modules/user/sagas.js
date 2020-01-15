f473e4e87e5b434b1132eb1016a68850c888ff92
renatopcinfo <renatopcinfo@gmail.com>
2020-01-15 00:25:57 -0300

    profile

--- mobile/src/store/modules/user/sagas.js
+++ mobile/src/store/modules/user/sagas.js
@@ -1,18 +1,17 @@
-import {Alert} from 'react-native';
-import {takeLatest, call, put, all} from 'redux-saga/effects';
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '~/services/api';
 
-import {updateProfileSuccess, updateProfileFailure} from './actions';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
-export function* updateProfile({payload}) {
+export function* updateProfile({ payload }) {
   try {
-    const {name, email, avatar_id, ...rest} = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
 
